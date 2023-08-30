fe9560356f475bd4bb13e3fc96b69a25e562d65c
DoctorAster <140829681+DoctorAster@users.noreply.github.com>
2023-08-30 19:59:15 +0600

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,3 +1,8 @@
-const button = document.getElementById("hello");
+const button = document.getElementById("name_button");
 button.addEventListener("click", AcceptName);
 //button.getAttribute("hidden");
+
+
+function AcceptName() {
+button.getAttribute("hidden");
+}
