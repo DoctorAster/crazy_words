9fac09731f957db16ce7d7f839dfcfaf7b9b6308
DoctorAster <140829681+DoctorAster@users.noreply.github.com>
2023-08-30 22:14:48 +0600

    Update chat.js

--- chat.js
+++ chat.js
@@ -3,8 +3,15 @@ const button.getElementById("name_button");
 button.addEventListener("click", AcceptName);
 
 function AcceptName() {
-  name_div = document.getElementById("hello");
-  name_input = document.getElementById("name");
-  name_div.setAttribute("hidden", "hidden");
-  user_name=name_input.value;
+    hello_div = document.getElementById("hello");
+    name_input = document.getElementById("name");
+    hello_name_div=document.getElementById("hello_name_div");
+    hello_name=document.getElementById("hello_name");
+    user_name=name_input.value;
+    attribute=hello_div.setAttribute("hidden", "hidden");
+    hello_name.innerText="Приветствуем Вас, " + user_name;
+    hello_name_div.removeAttribute("hidden");
+    check_text=hello_name.innerText;
+
 }
+
