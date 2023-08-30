6f8d34717979ddad9308283e693458ab6dc2f6b5
DoctorAster <140829681+DoctorAster@users.noreply.github.com>
2023-08-31 02:57:47 +0600

    Name can`t be empty

--- chat.js
+++ chat.js
@@ -7,8 +7,14 @@ function AcceptName() {
     hello_name_div=document.getElementById("hello_name_div");
     hello_name=document.getElementById("hello_name");
     user_name=name_input.value;
+    if (user_name.trim() == "") {
+        alert("Поле с именем не может быть пустым!")
+    }
+    else {
     attribute=hello_div.setAttribute("hidden", "hidden");
     hello_name.innerText="Приветствуем Вас, " + user_name;
     hello_name_div.removeAttribute("hidden");
     check_text=hello_name.innerText;
+    }
+    
 }
