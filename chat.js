2ada53a9ff163014c6be04e6e74143f9543fcfd5
DoctorAster <140829681+DoctorAster@users.noreply.github.com>
2023-09-01 19:01:40 +0600

    Update chat.js

--- chat.js
+++ chat.js
@@ -5,22 +5,28 @@ button.addEventListener("click", AcceptName);
 snd_button.addEventListener("click", SendMessage);
 
 function AcceptName() {
-    hello_div = document.getElementById("hello");
     name_input = document.getElementById("name");
-    hello_name_div=document.getElementById("hello_name_div");
-    hello_name=document.getElementById("hello_name");
     user_name=name_input.value;
     if (user_name.trim() == "") {
         alert("Поле с именем не может быть пустым!")
-    }
+        }
     else {
-    attribute=hello_div.setAttribute("hidden", "hidden");
-    hello_name.innerText="Приветствуем Вас, " + user_name;
-    hello_name_div.removeAttribute("hidden");
-    check_text=hello_name.innerText;
-    }
+        hello_name.innerText="Приветствуем Вас, " + user_name;
+        hello_name=document.getElementById("hello_name");
+        
+        hello_name_div=document.getElementById("hello_name_div");
+        hello_name_div.removeAttribute("hidden");
+        check_text=hello_name.innerText;
+        
+        hello_div = document.getElementById("hello");    
+        attribute=hello_div.setAttribute("hidden", "hidden");
+
+        chat_div=document.getElementById("chat_div");
+        chat_div.removeAttribute("hidden");
+        }
+    }  
 function SendMessage () {
     message = document.getElementById("message").value;
     document.getElementById("message").value = "";
     
-}
+    }
